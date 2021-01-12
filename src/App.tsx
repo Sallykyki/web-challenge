29a880ac49af784fa78e59c517c3737ca0c278d5
Huong Pham <huong.pq12@gmail.com>
2021-01-12 14:05:56 +0200

    feature: chore: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./styles/app.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
 
 function App() {
-  return <div className="App">this is a new app</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <LoginPage />
+          </Route>
+          <Route path="/list"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
