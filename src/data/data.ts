612b315878d733bf06d792e54e6213ab60c54caf
Huong Pham <huong.pq12@gmail.com>
2021-01-13 09:03:15 +0200

    fix: edit sample data

--- src/data/data.ts
+++ src/data/data.ts
@@ -14,31 +14,110 @@ const customers = [
 
 const jobs = [
   {
-    name: "complete proposal",
+    name: "Smart library project",
     id: "111",
-    dateStart: "1.1.2021",
-    dateEnd: "2.1.2021",
-    status: " Closed",
+    dateStart: "01.01.2021",
+    dateEnd: "12.01.2021",
+    status: "closed",
     customerId: "c02",
     username: "virvetk",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et neque dolor. Aenean in dolor",
+    tasks: [
+      {
+        name: "complete proposal",
+        dueDate: "02.01.2021",
+      },
+      {
+        name: "deliver first mockup",
+        dueDate: "05.01.2021",
+      },
+    ],
   },
+
   {
-    name: "deliver mockup",
+    name: "Flower and Sky project",
     id: "222",
-    dateStart: "5.1.2021",
-    dateEnd: "14.1.2021",
-    status: " Ongoing",
+    dateStart: "05.01.2021",
+    dateEnd: "14.01.2021",
+    status: "ongoing",
     customerId: "c03",
     username: "rekotk",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et neque dolor. Aenean in dolor",
+    tasks: [
+      {
+        name: "complete proposal",
+        dueDate: "02.01.2021",
+      },
+      {
+        name: "deliver first mockup",
+        dueDate: "05.01.2021",
+      },
+    ],
   },
   {
-    name: "deliver final product",
+    name: "Delivery Food mobile app",
     id: "333",
-    dateStart: "9.1.2021",
+    dateStart: "09.1.2021",
     dateEnd: "19.1.2021",
-    status: " Pending",
+    status: "pending",
     customerId: "c01",
     username: "seppotk",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et neque dolor. Aenean in dolor",
+    tasks: [
+      {
+        name: "complete proposal",
+        dueDate: "02.01.2021",
+      },
+      {
+        name: "deliver first mockup",
+        dueDate: "05.01.2021",
+      },
+    ],
+  },
+  {
+    name: "Love me love my dog project",
+    id: "444",
+    dateStart: "01.01.2021",
+    dateEnd: "02.01.2021",
+    status: "ongoing",
+    customerId: "c04",
+    username: "virvetk",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et neque dolor. Aenean in dolor",
+    tasks: [
+      {
+        name: "complete proposal",
+        dueDate: "02.01.2021",
+      },
+      {
+        name: "deliver first mockup",
+        dueDate: "05.01.2021",
+      },
+    ],
+  },
+  {
+    name: "VR travel app",
+    id: "345",
+    dateStart: "01.01.2021",
+    dateEnd: "21.1.2021",
+    status: "pending",
+    customerId: "c05",
+    username: "virvetk",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et neque dolor. Aenean in dolor",
+    tasks: [
+      {
+        name: "complete proposal",
+        dueDate: "02.01.2021",
+      },
+      {
+        name: "deliver first mockup",
+        dueDate: "05.01.2021",
+      },
+    ],
   },
 ];
 export { users, customers, jobs };
